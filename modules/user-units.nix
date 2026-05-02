d7a780534cbc39ada7df98aba6b2073867d7914b
Ubuntu <ubuntu@ip-172-31-6-25.us-west-2.compute.internal>
2026-05-02 19:44:33 +0000

    deploy subtitle bridge: add unit to system-manager manifest

--- modules/user-units.nix
+++ modules/user-units.nix
@@ -16,6 +16,7 @@ in
     // unit "sleet1213-hud-poller"
     // unit "sleet1213-irc"
     // unit "sleet1213-mc-bridge"
+    // unit "sleet1213-subtitle-bridge"
     // unit "sleet1213-temporal"
     // unit "sleet1213-webhook"
     // unit "sleet1213-worker";
